e8db0459ab7423f7b4eccd4755da1ea51e878601
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2023-03-13 21:21:55 +0530

    adding date and time

--- System Resource Monitor/Dashboard.Designer.cs
+++ System Resource Monitor/Dashboard.Designer.cs
@@ -29,18 +29,30 @@
         private void InitializeComponent()
         {
             this.pnl_left = new System.Windows.Forms.Panel();
-            this.pnl_headding = new System.Windows.Forms.Panel();
-            this.heading = new System.Windows.Forms.Label();
-            this.pnl_right = new System.Windows.Forms.Panel();
+            this.pnl_button = new System.Windows.Forms.Panel();
+            this.btn_ram = new System.Windows.Forms.Button();
+            this.btn_cpu = new System.Windows.Forms.Button();
             this.pnl_category = new System.Windows.Forms.Panel();
             this.btn_dashboard = new System.Windows.Forms.Button();
-            this.pnl_button = new System.Windows.Forms.Panel();
-            this.btn_cpu = new System.Windows.Forms.Button();
-            this.btn_ram = new System.Windows.Forms.Button();
+            this.pnl_headding = new System.Windows.Forms.Panel();
+            this.heading = new System.Windows.Forms.Label();
+            this.pnl_right_up = new System.Windows.Forms.Panel();
+            this.pnl_cpu = new System.Windows.Forms.Panel();
+            this.lbl_cpu = new System.Windows.Forms.Label();
+            this.pnl_right_down = new System.Windows.Forms.Panel();
+            this.pnl_time = new System.Windows.Forms.Panel();
+            this.pnl_date = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.pnl_left.SuspendLayout();
-            this.pnl_headding.SuspendLayout();
-            this.pnl_category.SuspendLayout();
             this.pnl_button.SuspendLayout();
+            this.pnl_category.SuspendLayout();
+            this.pnl_headding.SuspendLayout();
+            this.pnl_right_up.SuspendLayout();
+            this.pnl_cpu.SuspendLayout();
+            this.pnl_right_down.SuspendLayout();
+            this.pnl_time.SuspendLayout();
+            this.pnl_date.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl_left
@@ -52,38 +64,61 @@
             this.pnl_left.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnl_left.Location = new System.Drawing.Point(0, 0);
             this.pnl_left.Name = "pnl_left";
-            this.pnl_left.Size = new System.Drawing.Size(303, 521);
+            this.pnl_left.Size = new System.Drawing.Size(303, 495);
             this.pnl_left.TabIndex = 0;
             // 
-            // pnl_headding
+            // pnl_button
             // 
-            this.pnl_headding.Controls.Add(this.heading);
-            this.pnl_headding.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnl_headding.Location = new System.Drawing.Point(0, 0);
-            this.pnl_headding.Name = "pnl_headding";
-            this.pnl_headding.Size = new System.Drawing.Size(303, 100);
-            this.pnl_headding.TabIndex = 0;
+            this.pnl_button.AutoScroll = true;
+            this.pnl_button.Controls.Add(this.btn_ram);
+            this.pnl_button.Controls.Add(this.btn_cpu);
+            this.pnl_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_button.Location = new System.Drawing.Point(0, 140);
+            this.pnl_button.Name = "pnl_button";
+            this.pnl_button.Size = new System.Drawing.Size(303, 201);
+            this.pnl_button.TabIndex = 2;
             // 
-            // heading
+            // btn_ram
             // 
-            this.heading.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.heading.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.heading.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.heading.Location = new System.Drawing.Point(0, 0);
-            this.heading.Name = "heading";
-            this.heading.Size = new System.Drawing.Size(303, 100);
-            this.heading.TabIndex = 0;
-            this.heading.Text = "System Resource \r\nMonitor";
-            this.heading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_ram.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.btn_ram.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ram.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_ram.FlatAppearance.BorderSize = 0;
+            this.btn_ram.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_ram.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ram.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ram.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_ram.Location = new System.Drawing.Point(0, 50);
+            this.btn_ram.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_ram.Name = "btn_ram";
+            this.btn_ram.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btn_ram.Size = new System.Drawing.Size(303, 50);
+            this.btn_ram.TabIndex = 1;
+            this.btn_ram.Text = "RAM";
+            this.btn_ram.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_ram.UseVisualStyleBackColor = false;
+            this.btn_ram.Click += new System.EventHandler(this.btn_ram_Click);
             // 
-            // pnl_right
+            // btn_cpu
             // 
-            this.pnl_right.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
-            this.pnl_right.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnl_right.Location = new System.Drawing.Point(303, 0);
-            this.pnl_right.Name = "pnl_right";
-            this.pnl_right.Size = new System.Drawing.Size(690, 521);
-            this.pnl_right.TabIndex = 2;
+            this.btn_cpu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.btn_cpu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cpu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_cpu.FlatAppearance.BorderSize = 0;
+            this.btn_cpu.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_cpu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cpu.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cpu.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_cpu.Location = new System.Drawing.Point(0, 0);
+            this.btn_cpu.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_cpu.Name = "btn_cpu";
+            this.btn_cpu.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btn_cpu.Size = new System.Drawing.Size(303, 50);
+            this.btn_cpu.TabIndex = 0;
+            this.btn_cpu.Text = "CPU";
+            this.btn_cpu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_cpu.UseVisualStyleBackColor = false;
+            this.btn_cpu.Click += new System.EventHandler(this.btn_cpu_Click);
             // 
             // pnl_category
             // 
@@ -114,74 +149,130 @@
             this.btn_dashboard.UseVisualStyleBackColor = false;
             this.btn_dashboard.Click += new System.EventHandler(this.btn_dashboard_Click);
             // 
-            // pnl_button
+            // pnl_headding
             // 
-            this.pnl_button.AutoScroll = true;
-            this.pnl_button.Controls.Add(this.btn_ram);
-            this.pnl_button.Controls.Add(this.btn_cpu);
-            this.pnl_button.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnl_button.Location = new System.Drawing.Point(0, 140);
-            this.pnl_button.Name = "pnl_button";
-            this.pnl_button.Size = new System.Drawing.Size(303, 327);
-            this.pnl_button.TabIndex = 2;
+            this.pnl_headding.Controls.Add(this.heading);
+            this.pnl_headding.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_headding.Location = new System.Drawing.Point(0, 0);
+            this.pnl_headding.Name = "pnl_headding";
+            this.pnl_headding.Size = new System.Drawing.Size(303, 100);
+            this.pnl_headding.TabIndex = 0;
             // 
-            // btn_cpu
+            // heading
             // 
-            this.btn_cpu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-            this.btn_cpu.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_cpu.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_cpu.FlatAppearance.BorderSize = 0;
-            this.btn_cpu.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btn_cpu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cpu.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_cpu.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btn_cpu.Location = new System.Drawing.Point(0, 0);
-            this.btn_cpu.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_cpu.Name = "btn_cpu";
-            this.btn_cpu.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btn_cpu.Size = new System.Drawing.Size(303, 50);
-            this.btn_cpu.TabIndex = 0;
-            this.btn_cpu.Text = "CPU";
-            this.btn_cpu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_cpu.UseVisualStyleBackColor = false;
-            this.btn_cpu.Click += new System.EventHandler(this.btn_cpu_Click);
+            this.heading.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.heading.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heading.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.heading.Location = new System.Drawing.Point(0, 0);
+            this.heading.Name = "heading";
+            this.heading.Size = new System.Drawing.Size(303, 100);
+            this.heading.TabIndex = 0;
+            this.heading.Text = "System Resource \r\nMonitor";
+            this.heading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // btn_ram
+            // pnl_right_up
             // 
-            this.btn_ram.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-            this.btn_ram.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_ram.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_ram.FlatAppearance.BorderSize = 0;
-            this.btn_ram.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btn_ram.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_ram.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_ram.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btn_ram.Location = new System.Drawing.Point(0, 50);
-            this.btn_ram.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_ram.Name = "btn_ram";
-            this.btn_ram.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btn_ram.Size = new System.Drawing.Size(303, 50);
-            this.btn_ram.TabIndex = 1;
-            this.btn_ram.Text = "RAM";
-            this.btn_ram.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_ram.UseVisualStyleBackColor = false;
-            this.btn_ram.Click += new System.EventHandler(this.btn_ram_Click);
+            this.pnl_right_up.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.pnl_right_up.Controls.Add(this.pnl_cpu);
+            this.pnl_right_up.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_right_up.Location = new System.Drawing.Point(303, 0);
+            this.pnl_right_up.Name = "pnl_right_up";
+            this.pnl_right_up.Size = new System.Drawing.Size(568, 444);
+            this.pnl_right_up.TabIndex = 2;
+            // 
+            // pnl_cpu
+            // 
+            this.pnl_cpu.Controls.Add(this.lbl_cpu);
+            this.pnl_cpu.Location = new System.Drawing.Point(186, 173);
+            this.pnl_cpu.Name = "pnl_cpu";
+            this.pnl_cpu.Size = new System.Drawing.Size(316, 194);
+            this.pnl_cpu.TabIndex = 0;
+            // 
+            // lbl_cpu
+            // 
+            this.lbl_cpu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl_cpu.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cpu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_cpu.Location = new System.Drawing.Point(0, 0);
+            this.lbl_cpu.Name = "lbl_cpu";
+            this.lbl_cpu.Size = new System.Drawing.Size(316, 46);
+            this.lbl_cpu.TabIndex = 0;
+            this.lbl_cpu.Text = "CPU : ";
+            // 
+            // pnl_right_down
+            // 
+            this.pnl_right_down.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.pnl_right_down.Controls.Add(this.pnl_date);
+            this.pnl_right_down.Controls.Add(this.pnl_time);
+            this.pnl_right_down.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_right_down.Location = new System.Drawing.Point(303, 444);
+            this.pnl_right_down.Name = "pnl_right_down";
+            this.pnl_right_down.Size = new System.Drawing.Size(568, 51);
+            this.pnl_right_down.TabIndex = 3;
+            // 
+            // pnl_time
+            // 
+            this.pnl_time.Controls.Add(this.label1);
+            this.pnl_time.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_time.Location = new System.Drawing.Point(0, 0);
+            this.pnl_time.Name = "pnl_time";
+            this.pnl_time.Size = new System.Drawing.Size(297, 51);
+            this.pnl_time.TabIndex = 0;
+            // 
+            // pnl_date
+            // 
+            this.pnl_date.Controls.Add(this.label2);
+            this.pnl_date.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_date.Location = new System.Drawing.Point(297, 0);
+            this.pnl_date.Name = "pnl_date";
+            this.pnl_date.Size = new System.Drawing.Size(271, 51);
+            this.pnl_date.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(271, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Monitor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(993, 521);
-            this.Controls.Add(this.pnl_right);
+            this.ClientSize = new System.Drawing.Size(871, 495);
+            this.Controls.Add(this.pnl_right_down);
+            this.Controls.Add(this.pnl_right_up);
             this.Controls.Add(this.pnl_left);
             this.Name = "Monitor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "System Resource Monitor";
             this.Load += new System.EventHandler(this.Monitor_Load);
             this.pnl_left.ResumeLayout(false);
-            this.pnl_headding.ResumeLayout(false);
-            this.pnl_category.ResumeLayout(false);
             this.pnl_button.ResumeLayout(false);
+            this.pnl_category.ResumeLayout(false);
+            this.pnl_headding.ResumeLayout(false);
+            this.pnl_right_up.ResumeLayout(false);
+            this.pnl_cpu.ResumeLayout(false);
+            this.pnl_right_down.ResumeLayout(false);
+            this.pnl_time.ResumeLayout(false);
+            this.pnl_date.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -189,7 +280,7 @@
         #endregion
 
         private System.Windows.Forms.Panel pnl_left;
-        private System.Windows.Forms.Panel pnl_right;
+        private System.Windows.Forms.Panel pnl_right_up;
         private System.Windows.Forms.Label heading;
         private System.Windows.Forms.Panel pnl_headding;
         private System.Windows.Forms.Panel pnl_category;
@@ -197,6 +288,13 @@
         private System.Windows.Forms.Button btn_ram;
         private System.Windows.Forms.Button btn_cpu;
         private System.Windows.Forms.Button btn_dashboard;
+        private System.Windows.Forms.Panel pnl_cpu;
+        private System.Windows.Forms.Label lbl_cpu;
+        private System.Windows.Forms.Panel pnl_right_down;
+        private System.Windows.Forms.Panel pnl_date;
+        private System.Windows.Forms.Panel pnl_time;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
